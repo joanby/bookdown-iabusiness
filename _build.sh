8828dc479e6ad39d4447e3564a85a99de15983ce
Juan Gabriel <juangabriel@frogames.es>
2019-12-03 17:03:06 +0100

    automate script bash

--- _build.sh
+++ _build.sh
@@ -3,6 +3,12 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+printf 'Render BOOKDOWN listo'
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+printf 'Render PDF listo'
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+printf 'Render EPUB listo'
+
+mv -v /_book/* /docs/
+printf 'Contenido movido'
 
